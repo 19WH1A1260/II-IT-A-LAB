5d87ca28b18f04fbdcacac867ed8b6775ae89d91
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-06-18 11:15:35 +0530

    Add files via upload

---
+++ JP LAB/Exception Handling/ThrowException.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class ThrowsException
+{
+public static void validatevote(int age) 
+{
+if(age<18)
+{
+throw new ArithmeticException("not valid to vote");
+}
+else
+{
+System.out.println("Can cast the vote");
+}
+}
+public static void main(String args[])
+{
+System.out.println("Enter the age");
+Scanner sc = new Scanner(System.in);
+int age = sc.nextInt();
+validatevote(age);
+System.out.println("rest of the code");
+}
+}
+
