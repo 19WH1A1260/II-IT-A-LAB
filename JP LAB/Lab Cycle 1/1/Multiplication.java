3243ab1d55c29a5c1e6b85ea096bbb6cbec203e8
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-06-20 17:43:18 +0530

    Add files via upload

---
+++ JP LAB/Lab Cycle 1/1/Multiplication.java
@@ -0,0 +1,12 @@
+//Multiplication of two numbers
+public class Multiplication
+{
+public static void main(String args[])
+{
+int a, b, c;
+a = 5;
+b = 75;
+c = a * b;
+System.out.println("a * b = " + c);
+}
+}
