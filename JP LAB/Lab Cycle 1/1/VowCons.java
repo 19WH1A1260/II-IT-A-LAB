56d0e8c4978dde349117e28d7887e7ffb47d2cf3
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-06-20 17:45:35 +0530

    Add files via upload

---
+++ JP LAB/Lab Cycle 1/1/VowCons.java
@@ -0,0 +1,19 @@
+//check whether input character is vowel or consonant
+import java.util.*;
+public class VowCons
+{
+public static void main(String args[])
+{
+System.out.println("Enter a character");
+Scanner sc = new Scanner(System.in);
+char ch = sc.next().charAt(0);
+if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+{
+System.out.println(ch + " is vowel");
+}
+else
+{
+System.out.println(ch + " is consonant");
+}
+}
+}
