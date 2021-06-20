07ebdcd4220a1802ccc6781ef54e73fa41c5ec1b
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-06-20 17:41:03 +0530

    Add files via upload

---
+++ JP LAB/Lab Cycle 1/1/OddEven.java
@@ -0,0 +1,18 @@
+//Check if a number is odd or even
+public class OddEven
+{
+public static void main(String args[])
+{
+int num;
+num = 30;
+
+if(num % 2 == 0)
+{
+System.out.println("num is Even");
+}
+else
+{
+System.out.println("num is Odd");
+}
+}
+}
