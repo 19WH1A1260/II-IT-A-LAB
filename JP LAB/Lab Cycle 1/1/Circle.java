fc992e13539d6d4199c7e4100085e85e357d3022
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-06-20 17:42:01 +0530

    Add files via upload

---
+++ JP LAB/Lab Cycle 1/1/Circle.java
@@ -0,0 +1,14 @@
+//area and circumference of a circle
+public class Circle
+{
+public static void main(String args[])
+{
+int r;
+double area, circum;
+r = 10;
+area = 3.14 * r * r;
+circum = 2 * 3.14 * r;
+System.out.println("Area of circle = " + area);
+System.out.println("Circumference of circle = " + circum);
+}
+} 
