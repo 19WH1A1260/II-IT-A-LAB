0c90bc18017ba4fa7544235b25ac4d8fe43f5b52
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-06-25 15:27:24 +0530

    Add files via upload

---
+++ JP LAB/Lab Cycle 1/Arrays/ChararraytoString.java
@@ -0,0 +1,9 @@
+class ChararraytoString
+{
+public static void main(String[] args)
+{
+char[] ch={'T', 'o', 'm', 'h', 'i', 'd', 'd', 'l', 'e', 's', 't', 'o', 'n'};
+String str=new String(ch);
+System.out.print("string: "+str);
+}
+}
