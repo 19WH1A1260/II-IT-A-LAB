1aa459e7e6a8e6707be95a5043e413038caa73a8
Challakolusu Bhuvana <69143135+19WH1A1260@users.noreply.github.com>
2021-07-02 09:13:44 +0530

    Add files via upload

---
+++ JP LAB/Lab Cycle 1/Strings/ReverseString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ReverseString
+{
+public static void main(String[] args)
+{
+String str = "Harry potter";
+char a[] = str.toCharArray();
+for(int i=a.length-1;i>=0;i--)
+{
+System.out.print(a[i]);
+}
+}
+}
